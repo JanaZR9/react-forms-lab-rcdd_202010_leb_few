c42fbdb2fe617569c9989fb0de4fc382b322202c
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-31 11:32:25 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,19 +16,23 @@ class LoginForm extends React.Component {
     })
   }
 
+handleSubmit = e => {
+  
+}
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password} />
           </label>
         </div>
         <div>
